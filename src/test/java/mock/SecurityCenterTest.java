c8f56e1ff9ce2af2e4c3da728bb4d5897fbc6071
tanghaoran666 <18408230362@163.com>
2020-09-16 22:26:28 +0800

    implement mockito

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,17 +3,26 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
 
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
+    SecurityCenter securityCenter;
+    DoorPanel mockDoorPanel;
 
     @BeforeEach
     public void setUp() {
+        mockDoorPanel = mock(DoorPanel.class);
+        securityCenter = new SecurityCenter(mockDoorPanel);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+        verify(mockDoorPanel,times(1)).close();
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,34 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
+    GradeService gradeService;
+    GradeSystem mockGradeSystem;
 
+    @BeforeEach
+    void setUp(){
+        mockGradeSystem = mock(GradeSystem.class);
+        when(mockGradeSystem.gradesFor(isA(Long.class))).thenReturn(Arrays.asList(90.0, 95.0, 85.0));
+        gradeService = new GradeService(mockGradeSystem);
+    }
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
         //Assertions.assertEquals(90.0, result);
+        double result = gradeService.calculateAverageGrades(10L);
+        assertEquals(90.0,result);
     }
 }
